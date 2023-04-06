a0d5fbbe7eeff1c7e5502c8e36b02528b03c5708
Сергей Бернякович <sergejbernakovic@MacBook-Air-Sergej.local>
2023-04-06 10:12:51 +0500

    start

---
+++ src/shared/ui/Button.tsx
@@ -0,0 +1,5 @@
+import { Button } from '@mantine/core'
+
+export const AppButton = (props: any) => {
+	return <Button {...props}>{props.children}</Button>
+}
---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+import tsconfigPaths from 'vite-tsconfig-paths'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [react(), tsconfigPaths()],
+	server: {
+		port: 3000,
+		open: true,
+	},
+	resolve: {
+		alias: {
+			'~/shared/*': path.resolve(__dirname, './src/shared/*'),
+		},
+	},
+})
